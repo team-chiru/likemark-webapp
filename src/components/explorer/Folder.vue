e57c1f052ea87264a5f16c6c495b492fbfe51999
Jonathan Tower <joe.tourangeau@gmail.com>
2017-08-16 21:59:56 -0400

    Add component Folder and Link

---
+++ src/components/explorer/Folder.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="item">
+    <i class="item-primary">folder</i>
+    <div class="item-content">Folder 1</div>
+  </div>
+</template>
+
+<script>
+import Folder from './Folder.vue'
+import Link from './Link.vue'
+
+export default{
+  components: {
+    child: {
+      'folder': Folder,
+      'lien': Link
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+</style>
---
+++ src/components/explorer/Link.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="item">
+      <i class="item-primary">link</i>
+      <div class="item-content"><a href="https://www.google.com">https://www.google.com</a></div>
+    </div>
+</template>
+
+<script></script>
+
+<style lang="stylus">
+</style>
